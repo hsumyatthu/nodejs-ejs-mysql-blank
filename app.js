fe06c9c76b91ad8ee3a2e58099d5f7cf6c86e298
hsumyatthu <hsumyatthu.developer001@gmail.com>
2018-07-30 11:43:07 +0630

    Error Handler   -404 error page

--- app.js
+++ app.js
@@ -27,15 +27,22 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
+//client error for ajxa
+app.use(function(err, req, res, next){
+  if (req.xhr) {
+    res.status(err.status || 500).send(err.message);
+  }else {
+    next(err);
+  }
+});
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error' + ((err.status == 404)?'-404':''));
 });
 
 module.exports = app;
