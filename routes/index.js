5e53b38f89126d54bbd8abe20e92dc4593389b06
hsumyatthu <hsumyatthu.developer001@gmail.com>
2018-08-06 17:01:05 +0630

    Add/dupemail for signup

--- routes/index.js
+++ routes/index.js
@@ -25,13 +25,26 @@ router.post('/signup', function(req, res, next) {
       req.flash('warn', 'Duplicated email!!');
       res.redirect('/signup');
     }else {
-      User.add(params, function(err, result){
+      User.add(params, function(err2, result){
         if(err2) throw err2;
         res.redirect('/signin');
       });
     }
 });
 });
+
+/*Post email duplicated page */
+router.post('/dupemail', function(req, res, next) {
+  User.findByEmail(req.body.email, function(err, rows){
+    if (err) throw err;
+    if (rows.length > 0) {
+      res.json({ status: true, msg: 'Duplicated email!!'});
+    }else {
+      res.json({status: false});
+      }
+  });
+});
+
 /*Get signup page */
 router.get('/signin', function(req, res, next) {
   res.render('commons/sign-in', { title: 'Signin' });
