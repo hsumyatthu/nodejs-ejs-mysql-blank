726f35f87f6f6715c3822466a1f98b4ccb86ba0c
hsumyatthu <hsumyatthu.developer001@gmail.com>
2018-08-20 16:20:16 +0630

    Add user view for Admin

--- routes/admin/users.js
+++ routes/admin/users.js
@@ -10,4 +10,12 @@ router.all('/list', function(req, res, next) {
   });
 });
 
+router.get('/view/:id', function(req, res, next) {
+  User.findById( req.params.id,function(err, user) {
+    if (err) throw err;
+    if(user.length == 0) next(new Error('User data not found!'));
+    res.render('admin/users/user-view', { title: 'User View', user: user[0] });
+  });
+});
+
 module.exports = router;
