83ea62f7022fde975ea006ddb777ed81a27a5477
hsumyatthu <hsumyatthu.developer001@gmail.com>
2018-08-20 18:14:36 +0630

    Add user modify and delete

--- models/User.js
+++ models/User.js
@@ -7,15 +7,29 @@ var User = {
     params[2] = bcrypt.hashSync(params[2], bcrypt.genSaltSync(8), null);
     return db.query(sql, params, callback);
   },
+
   findByEmail: function(email, callback) {
     var params = [email];
     var sql = 'SELECT uid, email, password, name, role FROM users WHERE email = ?';
     return db.query(sql, [email], callback);
   },
+
   findById: function(uid, callback) {
     var sql = "SELECT uid, email, password, name, role, DATE_FORMAT(updated, '%d/%m/%Y %H:%i') AS updated FROM users WHERE uid = ?";
     return db.query(sql, [uid], callback);
   },
+
+  update: function(params, callback) {
+    var sql = "UPDATE users SET name =?, role = ?, updated = NOW() WHERE uid = ?";
+    return db.query(sql, params, callback);
+  },
+
+  remove: function(uid, callback) {
+    console.log('do');
+    var sql = "DELETE FROM users WHERE uid = ?";
+    return db.query(sql, [uid], callback);
+  },
+  
   find: function(params, callback) {
     var p = [];
     var sql = 'SELECT uid, email, password, name, role, DATE_FORMAT(updated, \'%d/%m/%Y %H:%i\') AS updated FROM users';
@@ -37,6 +51,7 @@ var User = {
     console.log(sql, p);
     return db.query(sql, p, callback);
   },
+
   compare: function(cleartext, encrypted) {
     return bcrypt.compareSync(cleartext, encrypted);
   }
--- routes/admin/users.js
+++ routes/admin/users.js
@@ -10,11 +10,40 @@ router.all('/list', function(req, res, next) {
   });
 });
 
-router.get('/view/:id', function(req, res, next) {
-  User.findById( req.params.id,function(err, user) {
+router.get('/view/:uid', function(req, res, next) {
+  User.findById( req.params.uid,function(err, user) {
     if (err) throw err;
     if(user.length == 0) next(new Error('User data not found!'));
     res.render('admin/users/user-view', { title: 'User View', user: user[0] });
+  });
+});
+
+router.get('/modify/:uid', function(req, res, next) {
+  User.findById( req.params.uid,function(err, user) {
+    if (err) throw err;
+    if(user.length == 0) next(new Error('User data not found!'));
+    res.render('admin/users/user-modify', { title: 'User View', user: user[0] });
+  });
+});
+
+router.post('/modify', function(req, res, next) {
+  var params = [req.body.name, req.body.role, req.body.uid];
+  User.findById( req.body.uid,function(err, user) {
+    if (err) throw err;
+    if(user.length == 0) next(new Error('User data not found!'));
+    User.update(params, function(uerr,uuser){
+      if(uerr) throw uerr;
+      req.flash('info', 'Success');
+      res.redirect('/admin/users/view/'+ user[0].uid);
+    });
+  });
+});
+
+router.post('/remove', function(req, res, next) {
+  User.remove(req.body.uid,function(err, user) {
+    if (err) throw err;
+    req.flash('info', 'Successfully');
+    res.redirect('/admin/users/list');
   });
 });
 
